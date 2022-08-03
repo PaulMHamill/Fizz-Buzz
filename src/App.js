6bb741ffbe758b44f266f2a9f2ddf285114998ee
Paul Hamill <enquiries@nestboxproperty.co.uk>
2022-08-03 12:01:47 +0100

    extension complete

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
 
   const [number, setNumber] = useState(1);
-  const [Output, setOutput] = useState(1)
+  const [Output, setOutput] = useState("")
+  const [unrelatedState, setUnrelatedState] = useState(0);
 
   useEffect(()=>{
     console.log("use effect triggered");
@@ -18,6 +19,11 @@ function App() {
       setOutput(number);
   }},[number]);
 
+  const unrelatedStateChanged = () => {
+    let randomNum = Math.floor(Math.random() * 100);
+    setUnrelatedState(randomNum)
+  }
+
   const handleInc = () => {
     setNumber(number + 1);
     }
@@ -28,12 +34,18 @@ function App() {
 
   return (
     <div className="App">
-      <button value={number} onClick={handleDec}> - </button>
-      <button value={number} onClick={handleInc}> + </button>
+      <button value={unrelatedState} onClick={handleDec}> - </button>
+      <button value={unrelatedState} onClick={handleInc}> + </button>
+
+      <button value={unrelatedState} onClick={unrelatedStateChanged}> change unrelated state </button>
+
+      <hr></hr>
 
       <h2> Number is {number}</h2>
 
       <h2> Answer is {Output} </h2>
+
+      <h3>unrelated state: <span>{unrelatedState}</span></h3>
 
       <button><a href="https://en.wikipedia.org/wiki/Fizz_buzz#:~:text=Players%20generally%20sit%20in%20a,by%2015%20become%20fizz%20buzz" >Fizz Buzz Instructions</a></button>
 
