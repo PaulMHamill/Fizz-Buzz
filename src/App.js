718b6d35c9277b93e0ca1d97dcee5573ce02f94e
Paul Hamill <enquiries@nestboxproperty.co.uk>
2022-08-03 11:10:42 +0100

    MVP complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const [number, setNumber] = useState(1);
+  const [Output, setOutput] = useState(1)
+
+  useEffect(()=>{
+    console.log("use effect triggered");
+    if (number % 3 === 0 && number % 5 === 0) {
+      setOutput("fizzbuz")
+    } else if (number % 3 === 0) {
+      setOutput("fizz")
+    } else if (number % 5 === 0) {
+      setOutput("buzz")
+    } else {
+      setOutput(number);
+  }},[number]);
+
+  const handleInc = () => {
+    setNumber(number + 1);
+    }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button value={number} onClick={handleInc}> + </button>
+
+      <h2> Number is {number}</h2>
+
+      <h2> {Output} </h2>
+
     </div>
   );
-}
+  }
 
 export default App;
